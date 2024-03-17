8dc906c0143fe7c2310eff921a3f83a7bb8cd49c
J <intellij@Js-MacBook-Pro.local>
2024-03-17 16:42:37 +0900

    create initial project

---
+++ src/main/java/com/project/idolInMyHand/IdolInMyHandApplication.java
@@ -0,0 +1,13 @@
+package com.project.idolInMyHand;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdolInMyHandApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdolInMyHandApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/idolInMyHand/IdolInMyHandApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.idolInMyHand;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdolInMyHandApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
